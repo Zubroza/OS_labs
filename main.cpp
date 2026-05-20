551546184806cdd34819249581ccaa8d806d4ad8
Цыбен <149075769+Zubroza@users.noreply.github.com>
2026-05-20 09:32:09 +0800

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdlib>
-#include "factorial.h"
-
-int main(int argc, char* argv[]) {
-    int n = std::atoi(argv[1]);
-    std::cout << n << "! = " << factorial(n) << std::endl;
-    return 0;
-}
